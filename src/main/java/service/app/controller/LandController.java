043876d8533284a2f265277c56bcf009877878fb
zwp <1258271806@qq.com>
2017-07-20 15:27:08 +0800

    添加TypeGet

--- src/main/java/service/app/controller/LandController.java
+++ src/main/java/service/app/controller/LandController.java
@@ -1,8 +1,19 @@
 package service.app.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+
+import service.app.server.LandDataService;
 
 @Controller
 public class LandController {
-
+	
+	@Autowired
+	LandDataService lds;
+	
+	
+	
+	
+	
+	
 }
---
+++ src/main/java/service/app/tramodel/TypeGetter.java
@@ -0,0 +1,53 @@
+package service.app.tramodel;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class TypeGetter {
+	
+	//道路客运客位数转客位类型
+	public String getRoadPassSitCotType(int sitCot){
+		//道路客运客位数转客位类型
+		return null;
+	}
+	
+	//道路客运企业规模获取
+	public String getRoadPassSitEntSizeType(int entS){
+		return null;
+	}
+	
+	public String getRoadPassDisType(Double tranDis){
+		//道路客运运距
+		return null;
+	}
+	
+	public String getRoadGoodsTonType(Double tonnage){
+		return null;
+	}
+	
+	
+	public String getRoadGoodsEntSizeType(int entS){
+		return null;
+	}
+	
+	public String getRoadGoodsDisType(Double tranDis){
+		//道路客运运距
+		return null;
+	}
+	
+	
+	public String getBusTranCarLenType(Double carLength){
+		//公交车车长
+		return null;
+	}
+	
+	public String getTaxiTranDpType(Double dpCot){
+		//楚竹车排量
+		return null;
+	}
+	
+	
+	
+	
+
+}
